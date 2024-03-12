e9a170436414ea6ca1544a1f890847478dd9868d
susnfor <97429786+Susnfor@users.noreply.github.com>
2024-03-12 13:47:43 +0000

    overflow

--- src/app/components/Showcase.tsx
+++ src/app/components/Showcase.tsx
@@ -15,7 +15,7 @@ export const Showcase = () => {
           <h1 className='font-bold text-4xl bg-gradient-to-r from-sky-500 to-indigo-500 bg-clip-text text-transparent'>What we offer</h1>
           <p className='text-xl text-extra'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vel purus ut velit tristique elementum.</p>
         </div>
-        <div id="showcase-img" className=' h-full pb-20 '>
+        <div id="showcase-img" className=' h-full pb-20 overflow-hidden '>
         {/* <Card title={'Web Design and Development'} details={'Details of the Service,Suspendisse ut magna porttitor, sollicitudin ligula at, molestie dolor. '} img={stock2}/> */}
         <ShowcaseCard source={stock2} left={true} phrase={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vel purus ut velit tristique elementum.'} />
         <ShowcaseCard source={stock1} left={false} phrase={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vel purus ut velit tristique elementum.'}/>
